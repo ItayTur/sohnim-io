a99b567060c4c1e3239a0b4a171cd38c9c33f64c
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2025-01-07 20:35:55 +0200

    feat: goes to lead details on lead row click

--- src/app/_components/Leads/LeadsTable/LeadsTable.tsx
+++ src/app/_components/Leads/LeadsTable/LeadsTable.tsx
@@ -2,11 +2,19 @@
 
 import { api } from "@/trpc/react";
 import { Box } from "@mui/material";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
 import { Table } from "../../UI";
+import { type Lead } from "../Leads.types";
 import { columns } from "./LeadsTable.columns";
 
 export const LeadsTable = () => {
   const { data: leads } = api.lead.getLeads.useQuery();
+  const router = useRouter();
+
+  const handleRowClick = async ({ row: lead }: { row: Lead }) => {
+    router.push(`/${lead.id}?${qs.stringify(lead)}`);
+  };
 
   return (
     <Box
@@ -15,6 +23,7 @@ export const LeadsTable = () => {
       <Table
         rows={leads}
         columns={columns}
+        onRowClick={handleRowClick}
         initialState={{
           sorting: {
             sortModel: [
