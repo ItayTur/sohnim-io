86a5e5f800f0a6dd20a426a03fab013a8cb5511b
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2025-01-19 20:24:34 +0200

    feat: adds error messages to transfer form

--- src/app/_components/TransferForm/TransferForm.consts.ts
+++ src/app/_components/TransferForm/TransferForm.consts.ts
@@ -1,3 +1,4 @@
+import { REQUIRED_MESSAGE } from "@/consts";
 import { z } from "zod";
 
 export enum TransferKeys {
@@ -12,14 +13,14 @@ export enum TransferKeys {
 }
 
 export const transferSchema = z.object({
-  [TransferKeys.NEW_PRODUCT_TYPE]: z.string(),
-  [TransferKeys.PREV_PRODUCT_TYPE]: z.string(),
-  [TransferKeys.NEW_COMPANY]: z.string(),
-  [TransferKeys.PREV_COMPANY]: z.string(),
-  [TransferKeys.NEW_MAIN_COURSE]: z.string(),
-  [TransferKeys.PREV_MAIN_COURSE]: z.string(),
+  [TransferKeys.NEW_PRODUCT_TYPE]: z.string().nonempty(REQUIRED_MESSAGE),
+  [TransferKeys.PREV_PRODUCT_TYPE]: z.string().nonempty(REQUIRED_MESSAGE),
+  [TransferKeys.NEW_COMPANY]: z.string().nonempty(REQUIRED_MESSAGE),
+  [TransferKeys.PREV_COMPANY]: z.string().nonempty(REQUIRED_MESSAGE),
+  [TransferKeys.NEW_MAIN_COURSE]: z.string().nonempty(REQUIRED_MESSAGE),
+  [TransferKeys.PREV_MAIN_COURSE]: z.string().nonempty(REQUIRED_MESSAGE),
   [TransferKeys.IS_DEFAULT]: z.boolean(),
-  [TransferKeys.SUBMISSION_ID]: z.string(),
+  [TransferKeys.SUBMISSION_ID]: z.string().nonempty(REQUIRED_MESSAGE),
 });
 
 export type TransferFormValues = z.infer<typeof transferSchema>;
